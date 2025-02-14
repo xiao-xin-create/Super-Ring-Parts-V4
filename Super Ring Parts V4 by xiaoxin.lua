57c384d45c9d77f42f9361442f27c68948c2e20a
xiao-xin-create <3928145450@qq.com>
2025-02-14 21:59:46 +0800

    Update Super Ring Parts V4 by xiaoxin.lua

--- Super Ring Parts V4 by xiaoxin.lua
+++ Super Ring Parts V4 by xiaoxin.lua
@@ -1,4 +1,4 @@
-"local Players = game:GetService("Players")
+local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local LocalPlayer = Players.LocalPlayer
